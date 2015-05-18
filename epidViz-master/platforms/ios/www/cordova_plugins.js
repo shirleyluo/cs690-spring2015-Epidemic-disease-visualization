fe0830f3634c3f21c0c80777c247bd5baebc4da1
Shirley Luo <xueluo@Xues-MacBook-Pro.local>
2015-05-18 00:00:29 -0700

    add compare

--- epidViz-master/platforms/ios/www/cordova_plugins.js
+++ epidViz-master/platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,41 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
         "id": "org.apache.cordova.statusbar.statusbar",
         "clobbers": [
@@ -11,6 +46,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "org.apache.cordova.device": "0.3.0",
+    "org.apache.cordova.geolocation": "0.3.12",
     "org.apache.cordova.statusbar": "0.1.10"
 }
 // BOTTOM OF METADATA
